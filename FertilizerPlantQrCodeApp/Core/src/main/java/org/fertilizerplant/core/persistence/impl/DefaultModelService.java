0efad988dc66c65c67c5e2b6e1104b61eddd6dd7
Noggie <930704958@qq.com>
2016-06-23 23:11:54 +0800

    added DefaultModelService

--- FertilizerPlantQrCodeApp/Core/src/main/java/org/fertilizerplant/core/persistence/impl/DefaultModelService.java
+++ FertilizerPlantQrCodeApp/Core/src/main/java/org/fertilizerplant/core/persistence/impl/DefaultModelService.java
@@ -1,12 +1,12 @@
 package org.fertilizerplant.core.persistence.impl;
 
 import org.fertilizerplant.core.persistence.ModelService;
+import org.fertilizerplant.core.persistence.utils.HiberanteUtils;
 
 public class DefaultModelService implements ModelService{
 	
 	public void save(Object target)
 	{
-		
 	}
 	public void create(Object target)
 	{
