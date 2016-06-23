2ff9a6f349899345a588a6d22319b57604aefefa
Noggie <930704958@qq.com>
2016-06-23 20:38:09 +0800

    added qrcode generation service

--- FertilizerPlantQrCodeApp/QrCodeService/src/main/java/org/fertilizerplant/qrcode/service/QrCodeGenerateService.java
+++ FertilizerPlantQrCodeApp/QrCodeService/src/main/java/org/fertilizerplant/qrcode/service/QrCodeGenerateService.java
@@ -3,6 +3,7 @@ package org.fertilizerplant.qrcode.service;
 import java.io.File;
 
 public interface QrCodeGenerateService {
-	
-	public void GenerateQrCode(File outputFile);
+
+	public void generateQrCode(final String outputFilePath, final String textToBeEncoded, final String fileType,
+			final int size);
 }
