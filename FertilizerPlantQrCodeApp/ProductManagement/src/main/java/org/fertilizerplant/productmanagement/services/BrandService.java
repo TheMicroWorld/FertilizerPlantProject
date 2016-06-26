710b0417f5cc7ee2802784af54898bb15a5e5c7b
Noggie <930704958@qq.com>
2016-06-26 11:57:56 +0800

    (1)added spring boot dependency(2)added product,brand and qrcodesequence service and repository

---
+++ FertilizerPlantQrCodeApp/ProductManagement/src/main/java/org/fertilizerplant/productmanagement/services/BrandService.java
@@ -0,0 +1,11 @@
+package org.fertilizerplant.productmanagement.services;
+
+import java.util.List;
+
+import org.fertilizerplant.productmanagement.models.Brand;
+
+public interface BrandService {
+	
+	List<Brand> getAllBrands();
+
+}
---
+++ FertilizerPlantQrCodeApp/ProductManagement/src/main/java/org/fertilizerplant/productmanagement/services/ProductService.java
@@ -0,0 +1,10 @@
+package org.fertilizerplant.productmanagement.services;
+
+import java.util.List;
+
+import org.fertilizerplant.productmanagement.models.Product;
+
+public interface ProductService {
+	
+	List<Product> getAllProducts();
+}
