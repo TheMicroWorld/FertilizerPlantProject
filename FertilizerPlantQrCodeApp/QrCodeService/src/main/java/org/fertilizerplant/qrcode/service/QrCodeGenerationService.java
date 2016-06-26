5bab1c79aea2df68164bdda77b67f169abfe8c2e
Noggie <930704958@qq.com>
2016-06-26 12:27:23 +0800

    rename project qrcodeservice to qrcodemanagement

--- FertilizerPlantQrCodeApp/QrCodeService/src/main/java/org/fertilizerplant/qrcode/service/QrCodeGenerationService.java
+++
@@ -1,9 +0,0 @@
-package org.fertilizerplant.qrcode.service;
-
-import java.io.File;
-
-public interface QrCodeGenerateService {
-
-	public void generateQrCode(final String outputFilePath, final String textToBeEncoded, final String fileType,
-			final int size);
-}
